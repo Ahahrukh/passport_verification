90ea1a29a222d46210bf9342a37c22bdf266811f
Ahahrukh <rose.sa818@gmail.com>
2024-10-02 23:17:19 +0530

    added the code agaian

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose' ;
 import CONSTANTS from './constants.js';
 
-const Database = mongoose.connect(CONSTANTS.database_url , { useNewUrlParser: true, useUnifiedTopology: true })
+const Database = mongoose.connect(CONSTANTS.database_url)
 
 export default Database ;
--- src/routes/user_auth_route.js
+++ src/routes/user_auth_route.js
@@ -1,11 +1,6 @@
 import { Router } from "express";
 import requestValidatorMiddleware from "../middleware/validator_middleware.js";
 import { createUserController, loginUserController, verificationController } from "../controllers/user_auth_controller.js";
-import multer from 'multer';
-import { GridFsStorage } from "multer-gridfs-storage";
-import crypto from 'crypto';
-import path from 'path';
-import CONSTANTS from "../config/constants.js";
 
 const userRouter = Router()
 
